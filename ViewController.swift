7d680ce6d547d8c90e80fa96670847d9fb03f9b9
qwienke <103984501+qwienke@users.noreply.github.com>
2023-02-21 15:04:10 -0700

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,27 @@
+////
+////  ViewController.swift
+////  familyProject
+////
+////  Created by Quinn Wienke on 2/13/23.
+////
+//
+//Family Project!
+//Create an app that introduces us to not only yourself, but your family as well.
+//Create a dashboard screen that shows us your family as a whole.
+//Need individual elements that we can tap on that will segue to a detail view about each individual family member.
+//Be creative with your design of the app! Personalize so it is your own. Add colors, fonts, pictures, etc.
+//Use constraints!
+//Create only 2 views. A dashboard view and a detail view. Dynamically populate the detail view dependant on what element is tapped in the dashboard view.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ purpleViewController.swift
@@ -0,0 +1,29 @@
+//
+//  purpleViewController.swift
+//  familyProject
+//
+//  Created by Quinn Wienke on 2/13/23.
+//
+
+import UIKit
+
+class purpleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
